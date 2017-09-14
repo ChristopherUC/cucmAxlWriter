440de4c1bad8edaad189e2f165eeee0190a2960d
Christopher Phillips <31781367+ChristopherUC@users.noreply.github.com>
2017-09-14 14:32:06 -0700

    changed logger instance name to reduce confusion

--- cucmAxlWriter.py
+++ cucmAxlWriter.py
@@ -12,17 +12,17 @@ from requests.auth import HTTPBasicAuth
 import urllib3  # imported to disable the SAN warning for the cert
 urllib3.disable_warnings(urllib3.exceptions.SubjectAltNameWarning)
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+cawLogger = logging.getLogger(__name__)
+cawLogger.setLevel(logging.DEBUG)
 handler = logging.FileHandler('writerDebug.log', mode='w')
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - \
                                 %(message)s')
 handler.setFormatter(formatter)
-logger.addHandler(handler)
-# logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.info("Begin cucmAxlWriter Info Logging")
-logger.debug("Begin cucmAxlWriter Debug Logging")
+cawLogger.addHandler(handler)
+# cawLogger.addHandler(logging.StreamHandler(sys.stdout))
+cawLogger.info("Begin cucmAxlWriter Info Logging")
+cawLogger.debug("Begin cucmAxlWriter Debug Logging")
 
 
 class cucmAxlWriter:
@@ -34,16 +34,17 @@ class cucmAxlWriter:
     def __init__(self):
         myCucmConfig = cucmAxlConfig()
 
-        logger.debug("current CUCM username is %s",
-                     myCucmConfig.getCucmUsername())
-        logger.debug("current CUCM password is %s",
-                     myCucmConfig.getCucmPassword())
-        logger.debug("current CUCM url is %s", myCucmConfig.getCucmUrl())
-        logger.debug("current CUCM axlurl is %s", myCucmConfig.getCucmAxlUrl())
-        logger.debug("current CUCM verify mode is %s",
-                     myCucmConfig.getCucmVerify())
-        logger.debug("current CUCM Cert File  is %s",
-                     myCucmConfig.getCucmCert())
+        cawLogger.debug("current CUCM username is %s",
+                        myCucmConfig.getCucmUsername())
+        cawLogger.debug("current CUCM password is %s",
+                        myCucmConfig.getCucmPassword())
+        cawLogger.debug("current CUCM url is %s", myCucmConfig.getCucmUrl())
+        cawLogger.debug("current CUCM axlurl is %s",
+                        myCucmConfig.getCucmAxlUrl())
+        cawLogger.debug("current CUCM verify mode is %s",
+                        myCucmConfig.getCucmVerify())
+        cawLogger.debug("current CUCM Cert File  is %s",
+                        myCucmConfig.getCucmCert())
 
         zeeplogger = logging.getLogger('zeep.transports')
         zeeplogger.setLevel(logging.DEBUG)
@@ -61,27 +62,27 @@ class cucmAxlWriter:
 
         session = Session()
         if myCucmConfig.getCucmVerify():
-            logger.info("Session Security ENABLED")
+            cawLogger.info("Session Security ENABLED")
             session.verify = myCucmConfig.getCucmCert()
         else:
-            logger.info("Session Security DISABLED")
+            cawLogger.info("Session Security DISABLED")
             session.verify = myCucmConfig.getCucmVerify()
-        logger.info("Session Created")
+        cawLogger.info("Session Created")
         session.auth = HTTPBasicAuth(myCucmConfig.getCucmUsername(),
                                      myCucmConfig.getCucmPassword())
-        logger.info("Auth Created")
+        cawLogger.info("Auth Created")
 
         client = Client(wsdl=myCucmConfig.getwsdlFileName(),
                         transport=Transport(session=session))
-        logger.info("Client Created")
+        cawLogger.info("Client Created")
 
         self.factory = client.type_factory('ns0')
-        logger.info("Factory Created")
+        cawLogger.info("Factory Created")
 
         self.service = client.create_service(
             "{http://www.cisco.com/AXLAPIService/}AXLAPIBinding",
             myCucmConfig.getCucmAxlUrl())
-        logger.info("Service Created")
+        cawLogger.info("Service Created")
 
     # TODO LDAP
         # check LDAP sync time (can this be checked?)
@@ -95,11 +96,11 @@ class cucmAxlWriter:
     def userGet(self, username):
         try:
             obtainedUser = self.service.getUser(userid=username)
-            logger.debug(obtainedUser)
+            cawLogger.debug(obtainedUser)
             return obtainedUser
         except Exception as e:
             # If user does not exist
-            logger.debug("User NOT found. Error=%s", e)
+            cawLogger.debug("User NOT found. Error=%s", e)
             return False
 
     def userExists(self, username):
@@ -107,7 +108,7 @@ class cucmAxlWriter:
             self.userGet(username)
             return True
         except Exception as e:
-            logger.debug("User NOT found. Error=%s", e)
+            cawLogger.debug("User NOT found. Error=%s", e)
             return False
 
     def userAdd(self, username):
@@ -122,8 +123,8 @@ class cucmAxlWriter:
                         associatedDevices={'device': deviceName},
                         primaryExtension={'pattern': extension,
                                           'routePartitionName': partition})
-        logger.info("Update User Completed")
-        logger.debug(result)
+        cawLogger.info("Update User Completed")
+        cawLogger.debug(result)
 
     def userDelete(self, username):
         return True
@@ -132,19 +133,19 @@ class cucmAxlWriter:
         try:
             getLine = self.service.getLine(pattern=extension,
                                            routePartitionName=partition)
-            logger.info("getLine Completed")
-            logger.debug(getLine)
+            cawLogger.info("getLine Completed")
+            cawLogger.debug(getLine)
             return getLine
         except Exception as e:
             return False
 
     def lineExists(self, extension, partition='Phones'):
-        logger.debug("lineExists Called")
+        cawLogger.debug("lineExists Called")
         try:
             getLine = self.service.getLine(pattern=extension,
                                            routePartitionName=partition)
-            logger.debug(getLine)
-            logger.info("Line Exists")
+            cawLogger.debug(getLine)
+            cawLogger.info("Line Exists")
             return True
         except Exception as e:
             return False
@@ -196,15 +197,15 @@ class cucmAxlWriter:
                 },
                 '''
 
-                logger.info("Line Factory Completed")
-                logger.debug(addlinepackage)
+                cawLogger.info("Line Factory Completed")
+                cawLogger.debug(addlinepackage)
 
                 createdLine = self.service.addLine(addlinepackage)
-                logger.debug("Line Created")
-                logger.debug(createdLine)
-                logger.info("Add Line Completed")
+                cawLogger.debug("Line Created")
+                cawLogger.debug(createdLine)
+                cawLogger.info("Add Line Completed")
             except Exception as e:
-                logger.debug("Add Line Error. Server error=%s", e)
+                cawLogger.debug("Add Line Error. Server error=%s", e)
                 raise Exception("Line could not be added")
         else:
             raise Exception("Line already exists")
@@ -237,8 +238,8 @@ class cucmAxlWriter:
     def deviceExists(self, devicename):
         try:
             getPhone = self.service.getPhone(name=devicename)
-            logger.info("getPhone Completed")
-            logger.debug(getPhone)
+            cawLogger.info("getPhone Completed")
+            cawLogger.debug(getPhone)
             return True
         except Exception as e:
             return False
@@ -273,13 +274,13 @@ class cucmAxlWriter:
                 tempDirN1 = self.factory.XDirn()
                 tempDirN1.pattern = extension
                 tempDirN1.routePartitionName = partition
-                logger.debug(tempDirN1)
+                cawLogger.debug(tempDirN1)
 
                 # PhoneLine is how a DirectoryNumber and a Phone are merged
                 tempPhoneLine1 = self.factory.XPhoneLine()
                 tempPhoneLine1.index = 1
                 tempPhoneLine1.dirn = tempDirN1
-                logger.debug(tempPhoneLine1)
+                cawLogger.debug(tempPhoneLine1)
 
                 addphonepackage = self.factory.XPhone()
                 addphonepackage.name = deviceName
@@ -293,14 +294,14 @@ class cucmAxlWriter:
                 addphonepackage.lines = {'line': tempPhoneLine1}
                 addphonepackage.ownerUserName = username
                 addphonepackage.callingSearchSpaceName = 'Device - ' + site
-                logger.debug(addphonepackage)
+                cawLogger.debug(addphonepackage)
 
                 createdPhone = self.service.addPhone(addphonepackage)
-                logger.debug(createdPhone)
-                logger.debug("Phone Created")
-                logger.info("Add Phone Completed")
+                cawLogger.debug(createdPhone)
+                cawLogger.debug("Phone Created")
+                cawLogger.info("Add Phone Completed")
             except Exception as e:
-                logger.debug("Add Phone Error. Server error=%s", e)
+                cawLogger.debug("Add Phone Error. Server error=%s", e)
                 # raise Exception("Phone could not be added")
                 raise Exception(e)
 
