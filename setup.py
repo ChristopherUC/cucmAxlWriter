60b1fac7bbda109d29c6033404915a830ca203f8
Christopher Phillips <31781367+ChristopherUC@users.noreply.github.com>
2017-09-15 14:23:55 -0700

    additional fields populated

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 setup(name='cucmAxlWriter',
       version='0.4',
+      description='cucm object writer using AXL',
+      author='Christopher Phillips',
+      author_email='christopherphillips+cucmAxlWriter@gmail.com',
+      url='https://github.com/ChristopherUC/cucmAxlWriter',
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
       py_modules=['cucmAxlWriter', 'cucmJabberWriter', 'cucmAxlConfig'],
       )
