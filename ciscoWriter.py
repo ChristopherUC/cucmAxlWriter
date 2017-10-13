dfe2d5f3e3f114c3d115e85d0d055feec559be5c
Christopher Phillips <31781367+ChristopherUC@users.noreply.github.com>
2017-10-12 22:45:02 -0700

    command line change

--- ciscoWriter.py
+++ ciscoWriter.py
@@ -62,7 +62,7 @@ parser.add_option("-m", "--snrdid", action="store", type="string",
 # myJabber = cucmJabberWriter('fernando', '5097148870', '118870',
 #                             'Seattle', 'True', 'NA', 'INTL', 'False', 'NA')
 '''./ciscoWriter.py -a create -u tdurden -e 223611 -d 2065551234 -s Tampa
- -h Northwoods -g 54321 -v True -b tdurden@apitest.org -p voicemailusertemplate
+ -i Northwoods -g 54321 -v True -b tdurden@apitest.org -p voicemailusertemplate
   -c International -r False -m 4255551212'''
 
 if not options.firstname:
